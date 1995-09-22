add3de16de754a427040e5963fe4daddfc16330a
fettig <fettig@3df82f5b-d43a-0410-af33-fce91db48ec5>
1995-09-22 10:23:56 +0000

    contains functions for reduction of history-selections: tsdb_verify_tuple tsdb_prepare_tree (transforms attribute names in relation/field                    descriptor of selection)
    git-svn-id: http://svn.delph-in.net/lkb/trunk@2066 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/tsdb/c/tsdb_tools.c
+++ src/tsdb/c/tsdb_tools.c
@@ -29,3 +29,70 @@ extern int errno;
 #include "tsdb.h"
 #include "errors.h"
 
+
+
+BOOL tsdb_verify_tuple(Tsdb_node* node, Tsdb_tuple** tuple ) {
+  BOOL left,right;
+  BYTE cmp;
+  int* desc;
+ 
+  if (node==NULL) 
+    return TRUE;
+  
+  if (node->node->type==TSDB_CONNECTIVE) {
+    left = tsdb_verify_tuple(node->left,tuple);
+    if ((node->node->value.connective==TSDB_AND)&&left) {
+      return tsdb_verify_tuple(node->right,tuple);
+    }
+    else /* AND with FALSE */
+      return FALSE;
+
+    if ((!left)&&(node->node->value.connective==TSDB_OR)) {
+      return tsdb_verify_tuple(node->right,tuple);
+    }
+    else /* OR with TRUE */
+      return TRUE;
+  } /* if connective */
+  
+  if (node->node->type==TSDB_OPERATOR) {
+    desc = node->left->node->value.descriptor;
+    if (tsdb_value_satisfies(tuple[desc[0]][desc[1]],
+                             node->node,
+                             node->right->node))
+      return TRUE;
+    else 
+      return FALSE;
+  } /* if operator */
+
+  return FALSE; /* I'm irritated */
+} /* tsdb_verify_tuple() */
+
+void tsdb_field_to_descriptor(Tsdb_selection* selection,Tsdb_node* n) {
+  int i,j;
+  Tsdb_relation* relation;
+
+  for (i=0;relation=selection->relations[i];i++) {
+    for (j=0;j<relation->n_fields;j++) {
+      if (!strcmp(relation->fields[j],n->node->value.string)) {
+        tsdb_free_tsdb_value(n->node);
+        n->node = tsdb_descriptor(i,j);
+      } /* if */         
+    } /* for */
+  } /* for */
+  
+} /* tsdb_field_to_descriptor() */
+
+Tsdb_node* tsdb_prepare_tree(Tsdb_node* node,Tsdb_selection* selection) {
+  Tsdb_node* the_node;
+  int r,f;
+  
+  if (node==NULL) 
+    return NULL;
+  if (node->node->type==TSDB_CONNECTIVE) {
+    tsdb_prepare_tree(node->left,selection);
+    tsdb_prepare_tree(node->right,selection);
+  }
+  if (node->node->type== TSDB_OPERATOR) {
+    tsdb_field_to_descriptor(selection,node->left);
+  }
+} /* tsdb_prepare_tree() */
