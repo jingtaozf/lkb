52069fea9ee01c6b9031ea604d89a468d6e1b261
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
1998-08-24 23:06:31 +0000

    fix handling of symbols from :tdl package use()d in TDL domains |:-{.
    git-svn-id: http://svn.delph-in.net/lkb/trunk@2392 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/mrs/page-interface.lisp
+++ src/mrs/page-interface.lisp
@@ -4,6 +4,15 @@
   ;;; allow mrsglobals-eng file to be system independent
   (intern (string-upcase str) lex:*lex-package*))
 
+;;
+;; unfortunately, all TDL domains have a few symbols from the :tdl package; the
+;; vsym() mechanism has no way to tell and, potentially, when we load() MRS
+;; the :disco domain is not yet defined |:-{.             (25-aug-98  -  oe)
+;;
+(eval-when #+:ansi-eval-when (:load-toplevel :compile-toplevel :execute)
+           #-:ansi-eval-when (load eval compile)
+  (import 'tdl::*diff-list* lex:*lex-package*))
+
 (defun get-parse-fs (parse)
   (lexicon::cfs-fs (car (main::typed-item-args parse))))
 
