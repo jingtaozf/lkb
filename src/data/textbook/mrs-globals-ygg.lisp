e5d77d2b61e5c4073cd16e519043e2aac5fc00b7
aac <aac@3df82f5b-d43a-0410-af33-fce91db48ec5>
1998-11-16 22:40:17 +0000

    globals for generation
    git-svn-id: http://svn.delph-in.net/lkb/trunk@2462 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ src/data/textbook/mrs-globals-ygg.lisp
@@ -0,0 +1,36 @@
+(in-package "MRS")
+;;; Globals for YGG
+
+(defparameter *initial-semantics-path* `(,(vsym "SEM") ))
+
+(defparameter *main-semantics-path* `(,(vsym "SEM") ,(vsym "RESTR") ,(vsym "LIST")))
+
+(defparameter *rel-name-path* `(,(vsym "RELN") ))
+
+(defparameter *rel-name-path-only* t)
+
+#|
+SEM [sem-struc
+     MODE ref
+     INDEX ref-index <8>
+     RESTR list-of-predications
+           LIST 
+                FIRST noun_pred
+                      RELN r_ande
+                      SIT sit-index
+                      INSTANCE <8>
+                REST <9>
+           LAST <9>
+|#
+
+(defparameter *value-feats* `(,(vsym "NAME")))
+
+(defparameter *reln-feature* (vsym "RELN"))
+
+(defparameter *psoa-handel-path* `(,(vsym "FOO")))
+(defparameter *psoa-top-h-path* `(,(vsym "FOO")))
+(defparameter *psoa-event-path* `(,(vsym "INDEX")))
+(defparameter *psoa-liszt-path* `(,(vsym "RESTR") ,(vsym "LIST")))
+(defparameter *psoa-rh-cons-path* `(,(vsym "FOO")))
+(defparameter *psoa-message-path* `(,(vsym "FOO")))
+(defparameter *psoa-wgliszt-path* `(,(vsym "FOO")))
