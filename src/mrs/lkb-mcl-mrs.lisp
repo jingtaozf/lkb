02aea81b84044d7de8f2a6af5bbdaa163ca6a1ba
johnca <johnca@3df82f5b-d43a-0410-af33-fce91db48ec5>
1999-12-22 16:10:24 +0000

    MRS display file for MCL
    git-svn-id: http://svn.delph-in.net/lkb/trunk@3013 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ src/mrs/lkb-mcl-mrs.lisp
@@ -0,0 +1,74 @@
+(in-package :cl-user)
+
+;;; MRS windows
+;;;
+;;; for MCL - John Carroll 21/12/99
+;;;
+;;; using standard text editing (Fred) windows
+
+(defparameter *show-mrs-prolog-p* t)
+
+(defun show-mrs-window (edge)
+  (let ((stream (make-instance 'fred-window
+                  :window-title
+                  (show-mrs-window-title edge
+                     (if *show-mrs-prolog-p* "Prolog MRS" "Simple MRS"))
+                  :scratch-p t))
+        (mrsstruct (mrs::extract-mrs edge)))
+    (if mrsstruct
+        (mrs::output-mrs1 mrsstruct
+           (if *show-mrs-prolog-p* 'mrs::prolog 'mrs::simple) stream)
+        (format stream "~%::: MRS structure could not be extracted~%"))
+    (show-mrs-update-window stream)))
+
+(defun show-mrs-indexed-window (edge)
+  (let ((stream (make-instance 'fred-window
+                  :window-title (show-mrs-window-title edge "Indexed MRS")
+                  :scratch-p t))
+        (mrsstruct (mrs::extract-mrs edge)))
+    (if mrsstruct
+        (mrs::output-mrs1 mrsstruct 'mrs::indexed stream)
+        (format stream "~%::: MRS structure could not be extracted~%"))
+    (show-mrs-update-window stream)))
+
+(defun show-mrs-scoped-window (edge)
+  (let* ((stream (make-instance 'fred-window
+                   :window-title (show-mrs-window-title edge "Scoped MRS")
+                   :scratch-p t))
+         (mrsstruct (mrs::extract-mrs edge))
+         (binding-sets (mrs::make-scoped-mrs mrsstruct)))
+    (if binding-sets
+        (for binding in binding-sets
+           do
+           (setf mrs::*canonical-bindings* (mrs::canonical-bindings binding))
+           (mrs::output-scoped-mrs mrsstruct :stream stream))
+        (format stream "~%::: MRS structure does not scope~%"))
+    (show-mrs-update-window stream)))
+
+
+(defun show-mrs-window-title (edge type)
+  (format nil "Edge ~A ~A - ~A"
+     (edge-id edge) (if (g-edge-p edge) "G" "P") type))
+
+(defun show-mrs-update-window (stream)
+  (file-position stream 0)
+  (force-output stream)
+  (fred-update stream))
+
+
+
+#|
+;;; nicer formatting for prolog-style MRS output
+
+(in-package "MRS")
+(defmethod mrs-output-start-rel ((mrsout prolog) sort handel)
+  (with-slots (stream need-rel-comma) mrsout
+    (when need-rel-comma (format stream ","))
+    (setf need-rel-comma t)
+    (format stream "~%  rel('~A',~A,[" (string-downcase sort) handel)))
+(defmethod mrs-output-start-h-cons ((mrsout prolog))
+  (with-slots (stream) mrsout
+    (format stream ",~%  hcons([")))
+(in-package :cl-user)
+
+|#
