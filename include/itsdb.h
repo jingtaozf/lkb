bb29cb0e9c52e0af48d4e7995cd466edc09f4e05
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2000-04-16 08:40:43 +0000

    [incr tsdb()] header files required to compile the LKB run-time module (PET).
    git-svn-id: http://svn.delph-in.net/lkb/trunk@3105 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ include/itsdb.h
@@ -0,0 +1,19 @@
+#define TASK_FAIL 42
+#define REMOTE_SHUTDOWN 43
+
+#define LISP_MESSAGE 50
+#define C_MESSAGE 51
+
+#define C_CREATE_RUN 0
+#define C_PROCESS_ITEM 1
+#define C_COMPLETE_RUN 2
+#define C_RECONSTRUCT_ITEM 3
+
+#define capi_putc(char) (capi_printf("%c", char) == 1 ? char : EOF)
+extern int capi_printf(char *format, ...);
+extern int capi_register(int (*)(char *, int, char *, int, char *),
+                         int (*)(int, char *, int, int, 
+                                 int, int, int),
+                         int (*)(char *),
+                         int (*)(int, char *));
+extern int slave(void);
