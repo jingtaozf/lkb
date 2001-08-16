b4e3f9b26e5c561695d287e5d8ca39f5ed2d729e
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2001-08-16 18:41:34 +0000

    new Tix 8.1.1 version.
    git-svn-id: http://svn.delph-in.net/lkb/trunk@3333 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ lib/tix8.1/pkgIndex.tcl
@@ -0,0 +1,7 @@
+# Tcl package index file, version 1.0
+#
+# $Id$
+#
+
+package ifneeded Tix 8.1.8.3 [list load "[file join [file dirname $dir] libtix8.1.8.3.a]" Tix]
+package ifneeded Tixsam 8.1.8.3 [list load "[file join [file dirname $dir] libtixsam8.1.8.3.a]" Tixsam]
