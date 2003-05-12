eeaf389fcdc1b5977937fe0fbfcbcf59c910d142
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-05-12 17:56:56 +0000

    improve elementary dependencies (even further).
    git-svn-id: http://svn.delph-in.net/lkb/trunk@3803 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/mrs/dependencies.lisp
+++ src/mrs/dependencies.lisp
@@ -1,31 +1,37 @@
 (in-package :mrs)
 
-(defparameter *dependencies-pretty-print-p* t)
+(defparameter *eds-pretty-print-p* t)
 
-(defparameter *dependencies-include-quantifiers-p* nil)
+(defparameter *eds-include-quantifiers-p* nil)
 
-(defparameter *dependencies-discourse-relation* (vsym "DISCOURSE_REL"))
+(defparameter *eds-include-vacuous-relations-p* nil)
 
-(defparameter *dependencies-message-relation* (vsym "MESSAGE"))
+(defparameter *eds-discourse-relation* (vsym "DISCOURSE_REL"))
 
-(defparameter %dependencies-eds% nil)
+(defparameter *eds-message-relation* (vsym "MESSAGE"))
 
-(defparameter %dependencies-variable-counter% 0)
+(defparameter %eds-eds% nil)
 
-(defparameter %dependencies-symbol-table% nil)
+(defparameter %eds-variable-counter% 0)
 
-(defparameter %dependencies-equivalences% nil)
+(defparameter %eds-symbol-table% nil)
 
-(defparameter %dependencies-relevant-features% 
-  '("ARG" "ARG1" "ARG2" "ARG3" "ARG4" "BV" "SOA" "RSTR" "BODY"
-    "MARG" "L-INDEX" "R-INDEX" "L-HANDEL" "R-HANDEL" "MAIN" "SUBORD" "ROLE"
+(defparameter %eds-representatives-table% nil)
+
+(defparameter %eds-equivalences% nil)
+
+(defparameter %eds-relevant-features% 
+  '("ARG" "ARG1" "ARG2" "ARG3" "ARG4" "BV" "SOA"
+    "CONST_VALUE" "CARG"
+    "MARG" "L-INDEX" "R-INDEX" 
+    "L-HNDL" "R-HNDL"  "L-HANDEL" "R-HANDEL" "MAIN" "SUBORD" "ROLE"
     "HINST" "NHINST"))
 
 (defstruct eds
   top relations hcons)
 
 (defmethod print-object ((object eds) stream)
-  (if *dependencies-pretty-print-p*
+  (if *eds-pretty-print-p*
     (loop
         initially
           (format 
@@ -33,7 +39,7 @@
            "{~@[~a:~]~@[ (cyclic)~]~@[~*~%~]" 
            (eds-top object) (ed-cyclic-p object) (eds-relations object))
         for ed in (eds-relations object)
-        unless (or (ed-bleached-p ed) (null (ed-arguments ed))) do
+        unless (or (ed-bleached-p ed) (ed-vacuous-p ed)) do
           (format stream " ~a~%" ed)
         finally
           (format stream "}~%"))
@@ -45,7 +51,7 @@
   raw)
 
 (defmethod print-object ((object ed) stream)
-  (if *dependencies-pretty-print-p*
+  (if *eds-pretty-print-p*
     (loop
         initially
           (format
@@ -76,19 +82,20 @@
         with index = (psoa-index psoa)
         with name = (when (var-p index) (var-name index))
         with hcons = (psoa-h-cons psoa)
-        with %dependencies-eds% = (make-eds :top name :hcons hcons)
-        with %dependencies-symbol-table% = (make-hash-table)
-        with %dependencies-equivalences% = (make-hash-table :test #'equal)
-        with %dependencies-variable-counter% = 0
+        with %eds-eds% = (make-eds :top name :hcons hcons)
+        with %eds-symbol-table% = (make-hash-table)
+        with %eds-representatives-table% = (make-hash-table)
+        with %eds-equivalences% = (make-hash-table :test #'equal)
+        with %eds-variable-counter% = 0
         for relation in (psoa-liszt psoa)
         for ed = (ed-convert-relation relation)
-        when ed do (push ed (eds-relations %dependencies-eds%))
+        when ed do (push ed (eds-relations %eds-eds%))
         finally
-          (setf (eds-relations %dependencies-eds%)
-            (nreverse (eds-relations %dependencies-eds%)))
-          (ed-bleach-eds %dependencies-eds%)
-          (ed-augment-eds %dependencies-eds%)
-          (return %dependencies-eds%))))
+          (setf (eds-relations %eds-eds%)
+            (nreverse (eds-relations %eds-eds%)))
+          (ed-bleach-eds %eds-eds%)
+          (ed-augment-eds %eds-eds%)
+          (return %eds-eds%))))
 
 (defun ed-convert-relation (relation)
   (let* ((handle (let ((handle (rel-handel relation)))
@@ -120,7 +127,7 @@
                           (when (member (fvpair-feature fvpair) soa :test #'eq)
                             (fvpair-value fvpair)))))
           (when (handle-var-p soa)
-            (setf (gethash handle %dependencies-equivalences%)
+            (setf (gethash handle %eds-equivalences%)
               (handle-var-name soa))
             (setf (ed-type ed) :message)))
       when (ed-quantifier-p ed) do (setf (ed-type ed) :quantifier)
@@ -131,9 +138,10 @@
       for ed in (eds-relations eds)
       unless (ed-bleached-p ed) do
         (loop
+            with old = (vsym "CONST_VALUE")
             with relation = (ed-raw ed)
             with flist = (rel-flist relation)
-            for feature in %dependencies-relevant-features%
+            for feature in %eds-relevant-features%
             for key = (vsym feature)
             for value = (loop
                             for fvpair in flist
@@ -142,11 +150,12 @@
                                 (fvpair-value fvpair)))
             for representative = (and value (ed-find-representative value))
             when representative do
-              (push (cons key representative) (ed-arguments ed)))
+              (push (cons (if (eq key old) (vsym "CARG") key) representative)
+                    (ed-arguments ed)))
         (setf (ed-arguments ed) (nreverse (ed-arguments ed)))))
 
 (defun ed-find-identifier (relation)
-  (or (gethash relation %dependencies-symbol-table%)
+  (or (gethash relation %eds-symbol-table%)
     (let* ((flist (and (rel-p relation) (rel-flist relation)))
            instance event)
       (loop
@@ -161,37 +170,54 @@
       (let* ((name (or
                     (and instance (var-p instance) (var-name instance))
                     (and event (var-p event) (var-name event))
-                    (format nil "_~d" (incf %dependencies-variable-counter%))))
+                    (format nil "_~d" (incf %eds-variable-counter%))))
              #+:null
-             (name (or (gethash name %dependencies-equivalences%) name)))
-        (setf (gethash relation %dependencies-symbol-table%) name)))))
+             (name (or (gethash name %eds-equivalences%) name)))
+        (setf (gethash relation %eds-symbol-table%) name)))))
 
-(defun ed-find-representative (variable)
-  (cond
-   ((handle-var-p variable)
-    (loop
-        with foo = (handle-var-name variable)
-        with name = (or (gethash foo %dependencies-equivalences%) foo)
-        with qeq = (or (ed-hcons-qeq name) (ed-hcons-qeq foo))
-        for ed in (eds-relations %dependencies-eds%)
-        for handle = (ed-handle ed)
-        when (and (not (ed-bleached-p ed))
-                  (or (equal name handle) (equal qeq handle)))
-        collect ed
-        into candidates
-        finally 
-          (return
-            (if (null (rest candidates))
-              (first candidates)
-              (ed-select-representative candidates)))))
-   ((var-p variable)
-    (loop
-        with foo = (var-name variable)
-        with name = (or (gethash foo %dependencies-equivalences%) foo)
-        for ed in (eds-relations %dependencies-eds%)
-        for id = (unless (ed-quantifier-p ed) (ed-id ed))
-        thereis (when (equal name id) ed)))
-   ((stringp variable) variable)))
+(defun ed-find-representative (variable &optional (selectp t))
+  (or (gethash variable %eds-representatives-table%)
+      (setf (gethash variable %eds-representatives-table%)
+        (cond
+         ((handle-var-p variable)
+          ;;
+          ;; _fix_me_
+          ;; needs transitive treatment for `kim slept while sandy arrived',
+          ;; when messages are bleached: the subordinating conjunction takes a
+          ;; handle argument, which is qeq-ed with a message handle, whose SOA
+          ;; is qeq-ed once again.                           (11-may-03; oe)
+          ;;
+          (loop
+              with foo = (handle-var-name variable)
+              with name = (or (ed-variable-equivalence foo) foo)
+              with qeq = (ed-hcons-qeq name)
+              for ed in (eds-relations %eds-eds%)
+              for handle = (ed-handle ed)
+              when (and (not (ed-bleached-p ed))
+                        (or (equal name handle) 
+                            (and (handle-var-p qeq) 
+                                 (equal (handle-var-name qeq) handle))))
+              collect ed
+              into candidates
+              finally 
+                (return
+                  (if selectp
+                    (if (and candidates (null (rest candidates)))
+                      (first candidates)
+                      (ed-select-representative
+                       (append
+                        (when (handle-var-p qeq)
+                          (ed-find-representative qeq nil))
+                          candidates)))
+                    candidates))))
+         ((var-p variable)
+          (loop
+              with foo = (var-name variable)
+              with name = (or (ed-variable-equivalence foo) foo)
+              for ed in (eds-relations %eds-eds%)
+              for id = (unless (ed-quantifier-p ed) (ed-id ed))
+              thereis (when (equal name id) ed)))
+         ((stringp variable) variable)))))
 
 (defun ed-select-representative (eds)
   ;;
@@ -206,7 +232,8 @@
    (loop
        for ed in eds
        unless (char= (char (ed-id ed) 0) #\_) collect ed into candidates
-       finally (when (null (rest candidates)) (return (first candidates))))
+       finally (when (and candidates (null (rest candidates)))
+                 (return (first candidates))))
    (loop
        for ed in eds
        for id = (ed-id ed)
@@ -229,34 +256,42 @@
   (loop
       with name = (if (stringp handle) handle (when (handle-var-p handle)
                                                 (handle-var-name handle)))
-      for hcons in (eds-hcons %dependencies-eds%)
+      for hcons in (eds-hcons %eds-eds%)
       for scarg = (when (eq (hcons-relation hcons) (vsym "QEQ"))
                     (handle-var-name (hcons-scarg hcons)))
       thereis 
-        (when (equal name scarg) 
-          (handle-var-name (hcons-outscpd hcons)))))
+        (when (equal name scarg) (hcons-outscpd hcons))))
+
+(defun ed-variable-equivalence (name)
+  (gethash name %eds-equivalences%))
 
 (defun ed-quantifier-p (ed)
   (let ((flist (rel-flist (ed-raw ed))))
     (find *scope-feat* flist :key #'fvpair-feature)))
 
 (defun ed-message-p (ed)
-  (when *dependencies-message-relation*
+  (when *eds-message-relation*
     (let ((type (ed-predicate ed)))
-      (or (eq type *dependencies-message-relation*)
-          (subtype-p type *dependencies-message-relation*)))))
+      (or (eq type *eds-message-relation*)
+          (subtype-p type *eds-message-relation*)))))
 
 (defun ed-discourse-p (ed)
-  (when *dependencies-discourse-relation*
+  (when *eds-discourse-relation*
     (let ((type (ed-predicate ed)))
-      (or (eq type *dependencies-discourse-relation*)
-          (subtype-p type *dependencies-discourse-relation*)))))
+      (or (eq type *eds-discourse-relation*)
+          (subtype-p type *eds-discourse-relation*)))))
 
 (defun ed-bleached-p (ed)
   (or 
    (member (ed-type ed) '(:message :discourse) :test #'eq)
-   (and (null *dependencies-include-quantifiers-p*)
+   (and (null *eds-include-quantifiers-p*)
         (eq (ed-type ed) :quantifier))))
+
+(defun ed-vacuous-p (ed)
+  (unless *eds-include-vacuous-relations-p*
+    (or (null (ed-arguments ed))
+        (and (null (rest (ed-arguments ed)))
+             (eq (first (first (ed-arguments ed))) (vsym "CARG"))))))
 
 (defun ed-wellformed-p (eds)
   (not (ed-cyclic-p eds)))
@@ -267,16 +302,11 @@
       unless (or (ed-bleached-p ed) (null (ed-arguments ed))) do
         (unless (ed-walk ed) (return ed))))
 
-(defun ed-walk (ed &optional (start ed startp))
-  (unless (and startp (eq ed start))
+(defun ed-walk (ed &optional (start (ed-id ed) startp))
+  (unless (and startp (eq (ed-id ed) start))
     (loop
-        for (role . value) in (ed-arguments ed)
-        when (and (ed-p value) (not (ed-bleached-p value))) do
+        for (role . value) in (unless (ed-bleached-p ed) (ed-arguments ed))
+        when (ed-p value) do
           (setf role role)
           (unless (ed-walk value start) (return nil))
         finally (return t))))
-
-
-
-
-
