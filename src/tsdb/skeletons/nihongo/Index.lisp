65a037a9472b4d6a0e1cbae695efd4294a36b1da
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-05-21 05:47:47 +0000

    include a couple of Japanese skeletons with [incr tsdb()] (donated by YY).
    git-svn-id: http://svn.delph-in.net/lkb/trunk@3828 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ src/tsdb/skeletons/nihongo/Index.lisp
@@ -0,0 +1,26 @@
+;;;
+;;; this file should be `Index.lisp' and reside in the directory containing the
+;;; tsdb(1) test suite skeleton databases (typically a subdirectory `skeletons'
+;;; in the tsdb(1) database root directory `*tsdb-home*').
+;;;
+;;; the file should contain a single un-quote()d Common-Lisp list enumerating
+;;; the available skeletons, e.g.
+;;;
+;;;   (((:path . "english") (:content . "English TSNLP test suite"))
+;;;    ((:path . "csli") (:content . "CSLI (ERGO) test suite"))
+;;;    ((:path . "vm") (:content . "English VerbMobil data")))
+;;;
+;;; where the individual entries are assoc() lists with at least two elements:
+;;;
+;;;   - :path --- the (relative) directory name containing the skeleton;
+;;;   - :content --- a descriptive comment.
+;;;
+;;; the order of entries is irrelevant as the `tsdb :skeletons' command sorts
+;;; the list lexicographically before output.
+;;;
+
+(((:path . "banira") (:content . "Basic Japanese Constructions"))
+ ((:path . "ginkougyou") (:content . "Hand-Constructed Banking Data"))
+ ((:path . "dates") (:content . "Date Expressions"))
+ ((:path . "numbers") (:content . "Number Expressions"))
+ ((:path . "faqs") (:content . "Online Banking FAQ Sample")))
