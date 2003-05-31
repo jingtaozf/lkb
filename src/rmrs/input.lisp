3d18f9384d115ef22870636044eaf037ca69737d
aac <aac@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-05-31 20:05:26 +0000

    XML input
    git-svn-id: http://svn.delph-in.net/lkb/trunk@3859 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ src/rmrs/input.lisp
@@ -0,0 +1,223 @@
+;;; Copyright (c) 2003
+;;;   John Carroll, Ann Copestake, Robert Malouf, Stephan Oepen;
+;;;   see `licence.txt' for conditions.
+
+(in-package :mrs)
+
+;;; reading in an rmrs file as specced in rmrs.dtd
+;;;
+;;; some of the fns are similar to those in readgram
+;;; but there are sufficient differences to make it preferable
+;;; to handle them separately
+
+#|
+(read-rmrs-file "test.xml")
+|#
+ 
+(defun read-rmrs-file (file-name)
+  ;;; <!ELEMENT rmrs-list (rmrs)*>
+  (with-open-file (istream file-name :direction :input)
+    (let ((rmrss (parse-xml-removing-junk istream)))
+      (setf *rmrs-debug* rmrss)
+      (unless (equal (car rmrss) '|rmrs-list|)
+        (error "~A is not a valid rmrs file" file-name))
+      (loop for rmrs in (cdr rmrss)
+          unless (xml-whitespace-string-p rmrs)
+          collect
+            (read-rmrs rmrs)))))
+
+(defun read-rmrs (content)
+;;; <!ELEMENT rmrs (label, (ep|rarg|ing|hcons)*)>
+  (let ((top-h nil) (eps nil) (rargs nil) (ings nil)
+        (h-cons nil))
+    (let ((tag (car content)))
+      (unless (eql tag '|rmrs|)
+        (error "~A is not an rmrs" content)))
+    (setf content (cdr content))
+    (loop (let ((next-el (car content)))
+            (if (xml-whitespace-string-p next-el)
+                (pop content)
+              (return))))
+    (when content
+      (setf top-h (read-rmrs-label (car content)))
+      (loop for next-el in (cdr content)
+          do
+            (unless (xml-whitespace-string-p next-el)
+              (let*  
+                  ((next-tag (car next-el))
+                   (body (cdr next-el)))
+                (cond 
+                 ((eql next-tag '|rarg|)
+                  (push (read-rmrs-rarg body)
+                        rargs))
+                 ((eql next-tag '|ing|)
+                  (push (read-rmrs-in-g body)
+                        ings))
+                 ((atom next-tag)
+                  (error "Unexpected element ~A" next-el))
+                 ((eql (car next-tag) '|ep|)
+                  (push (read-rmrs-ep next-el)
+                        eps))
+                 ((eql (car next-tag) '|hcons|)
+                  (push (read-rmrs-hcons next-el)
+                        h-cons))
+                 (t (error "Unexpected element ~A" next-el))))))
+      (make-rmrs :top-h top-h
+                 :liszt (nreverse eps)
+                 :h-cons (nreverse h-cons)
+                 :rmrs-args (nreverse rargs)
+                 :in-groups (nreverse ings)))))
+
+(defun read-rmrs-ep (content)
+;;; <!ELEMENT ep ((realpred|gpred), label, var)>
+;;; <!ATTLIST ep
+;;;          cfrom CDATA #REQUIRED
+;;;          cto   CDATA #REQUIRED > 
+  (let ((tag (car content))
+        (body (cdr content)))
+    (unless (and 
+             (eql (first tag) '|ep|)
+             (eql (second tag) '|cfrom|)
+             (eql (fourth tag) '|cto|))
+      (error "Malformed ep ~A" content))
+    (make-char-rel 
+     :sort (read-rmrs-pred (first body))
+              :handel (read-rmrs-label (second body))
+              :flist (list (read-rmrs-var (third body)))
+              :cfrom (parse-integer (third tag))
+              :cto (parse-integer (fifth tag)))))
+
+(defun read-rmrs-pred (content)
+  (let ((tag (car content))
+        (body (cdr content)))
+    (cond 
+     ((eql tag '|pred|) (make-dummy-pred))
+     ((eql tag '|gpred|) (car body))
+         ;;; <!ELEMENT gpred (#PCDATA)>
+     ((atom tag)
+      (error "Unexpected element ~A" content))
+     ((eql (car tag) '|realpred|)
+      (read-rmrs-real-pred tag))
+     (t (error "Unexpected element ~A" content)))))
+
+(defun read-rmrs-real-pred (tag)
+;;; <!ELEMENT realpred EMPTY>
+;;;
+;;; <!ATTLIST realpred
+;;;          lemma CDATA #REQUIRED
+;;;          pos (V|N|J|R|P) #IMPLIED
+;;;          sense CDATA #IMPLIED >
+    (unless (and 
+             (eql (first tag) '|realpred|)
+             (eql (second tag) '|lemma|))
+      (error "Malformed realpred ~A" tag))
+    (let ((pos-rest (member '|pos| tag))
+          (sense-rest (member '|sense| tag)))
+      (make-realpred :lemma (third tag)
+                      :pos (cadr pos-rest)
+                      :sense (cadr sense-rest))))
+
+
+(defun read-rmrs-label (content)
+;;; <!ELEMENT label EMPTY>
+;;;
+;;; <!ATTLIST label 
+;;;          vid CDATA #REQUIRED >
+  (let ((tag (car content))
+        (body (cdr content)))
+    (unless (and 
+             (eql (first tag) '|label|)
+             (eql (second tag) '|vid|)
+             (null body))
+      (error "Malformed label ~A" content))
+    (create-new-label-with-id (parse-integer (third tag)))))
+
+(defun create-new-label-with-id (idnumber)
+  (let* ((variable-name (format nil "h~A" idnumber)))
+        (make-handle-var 
+         :name variable-name
+         :type 'handle
+         :id idnumber)))
+
+
+(defun read-rmrs-var (content)
+;;; <!ELEMENT var EMPTY>
+;;; <!ATTLIST var
+;;;          sort (x|e|h|u|l) #REQUIRED
+;;;          vid  CDATA #REQUIRED 
+;;;          num  CDATA #IMPLIED
+;;;          pers CDATA #IMPLIED
+;;;          gender CDATA #IMPLIED
+;;;          tense CDATA #IMPLIED
+;;;          aspect CDATA #IMPLIED >
+  (let ((tag (car content))
+        (body (cdr content)))
+    (unless (and 
+             (eql (first tag) '|var|)
+             (eql (second tag) '|sort|)
+             (eql (fourth tag) '|vid|)
+             (null body))
+      (error "Malformed variable ~A" content))
+    (create-new-var-with-id (parse-integer (fifth tag)) 
+                            (third tag))))
+;;; FIX - do the `extra' stuff
+
+(defun create-new-var-with-id (idnumber str)
+  (let* ((type (find-var-type str))
+         (variable-name (format nil "~A~A" str idnumber)))
+        (make-var 
+         :name variable-name
+         :type type
+         :id idnumber)))
+
+(defun read-rmrs-constant (content)
+;;;  <!ELEMENT constant (#PCDATA)>
+  (read-rmrs-simple '|constant| content))
+  
+(defun read-rmrs-rarg (content)
+  ;;; <!ELEMENT rarg (rargname, label, (var|constant)) >
+  (let ((name nil) (label nil) (arg nil))
+    (setf name (read-rmrs-simple '|rargname| (car content)))
+    ;;; <!ELEMENT rargname (#PCDATA)>
+    (setf label (read-rmrs-label (cadr content)))
+    (let* ((argval (caddr content))
+           (argvaltag (car argval)))
+      (setf arg
+        (if (eql argvaltag '|constant|)
+            (read-rmrs-constant argval)
+          (read-rmrs-var argval))))
+    (make-rmrs-arg :arg-type name :label label 
+                   :val arg)))
+  
+(defun read-rmrs-in-g (content)
+;;; <!ELEMENT ing (ing-a,ing-b) >
+;;; <!ELEMENT ing-a (var)>
+;;; <!ELEMENT ing-b (var)>  
+  (let ((ing-a-xml (car content))
+        (ing-b-xml (cadr content)))
+    (unless (and (eql (car ing-a-xml) '|ing-a|)
+                 (eql (car ing-b-xml) '|ing-b|))
+      (error "Malformed ing ~A" content))
+    (make-in-group :labels (list (read-rmrs-var (cadr ing-a-xml))
+                                 (read-rmrs-var (cadr ing-b-xml))))))
+                                 
+
+(defun read-rmrs-hcons (content)
+;;; <!ELEMENT hcons (hi, lo)>
+;;; <!ATTLIST hcons 
+;;;          hreln (qeq|lheq|outscopes) #REQUIRED >
+;;;
+;;; <!ELEMENT hi (var)>
+;;; <!ELEMENT lo (label|var)>
+  (let ((tag (car content))
+        (body (cdr content)))
+    (unless (and (eql (first tag) '|hcons|)
+                 (eql (second tag) '|hreln|)
+                 (eql (first (first body)) '|hi|)
+                 (eql (first (second body)) '|lo|))
+      (error "Malformed hcons ~A" content))
+    (make-hcons :relation (third tag)
+                :scarg (read-rmrs-var (second (first body)))
+                :outscpd (read-rmrs-label (second (second body))))))
+                
+
