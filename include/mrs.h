93e5625f8524692db39805b0449e493b0f33f9c7
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-08-19 20:50:48 +0000

    add ECL-compiled version of MRS code to repository, for PET to link :-).
    git-svn-id: http://svn.delph-in.net/lkb/trunk@4085 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ include/mrs.h
@@ -0,0 +1,53 @@
+#include <ecl.h>
+
+#ifdef __cplusplus
+#define ECL_CPP_TAG "C"
+#else
+#define ECL_CPP_TAG
+#endif
+
+void
+initialize_mrs() {
+
+  extern ECL_CPP_TAG void init_LKB_PACKAGE(cl_object);
+  extern ECL_CPP_TAG void init_MRS_PACKAGE(cl_object);
+  extern ECL_CPP_TAG void init_ECL(cl_object);
+  extern ECL_CPP_TAG void init_PET_INTERFACE(cl_object);
+  extern ECL_CPP_TAG void init_MRSGLOBALS(cl_object);
+  extern ECL_CPP_TAG void init_BASEMRS(cl_object);
+  extern ECL_CPP_TAG void init_MRSOUTPUT(cl_object);
+  extern ECL_CPP_TAG void init_MRSCORPUS(cl_object);
+  extern ECL_CPP_TAG void init_INTERFACE(cl_object);
+  extern ECL_CPP_TAG void init_MRSRESOLVE(cl_object);
+  extern ECL_CPP_TAG void init_MRSCONS(cl_object);
+  extern ECL_CPP_TAG void init_CHEAPSCOPE(cl_object);
+  extern ECL_CPP_TAG void init_COMP(cl_object);
+  extern ECL_CPP_TAG void init_CONVERT(cl_object);
+  extern ECL_CPP_TAG void init_ANNLT(cl_object);
+  extern ECL_CPP_TAG void init_OUTPUT(cl_object);
+  extern ECL_CPP_TAG void init_READGRAM(cl_object);
+  extern ECL_CPP_TAG void init_READTAG(cl_object);
+  extern ECL_CPP_TAG void init_INPUT(cl_object);
+  extern ECL_CPP_TAG void init_DEPENDENCIES(cl_object);
+  read_VV(OBJNULL, init_LKB_PACKAGE);
+  read_VV(OBJNULL, init_MRS_PACKAGE);
+  read_VV(OBJNULL, init_ECL);
+  read_VV(OBJNULL, init_PET_INTERFACE);
+  read_VV(OBJNULL, init_MRSGLOBALS);
+  read_VV(OBJNULL, init_BASEMRS);
+  read_VV(OBJNULL, init_MRSOUTPUT);
+  read_VV(OBJNULL, init_MRSCORPUS);
+  read_VV(OBJNULL, init_INTERFACE);
+  read_VV(OBJNULL, init_MRSRESOLVE);
+  read_VV(OBJNULL, init_MRSCONS);
+  read_VV(OBJNULL, init_CHEAPSCOPE);
+  read_VV(OBJNULL, init_COMP);
+  read_VV(OBJNULL, init_CONVERT);
+  read_VV(OBJNULL, init_ANNLT);
+  read_VV(OBJNULL, init_OUTPUT);
+  read_VV(OBJNULL, init_READGRAM);
+  read_VV(OBJNULL, init_READTAG);
+  read_VV(OBJNULL, init_INPUT);
+  read_VV(OBJNULL, init_DEPENDENCIES);
+
+} /* initialize_mrs() */
