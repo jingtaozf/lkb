08e564be0c3b218cb4abc92043607128bdf03714
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-08-20 13:53:19 +0000

    ECL PET interface update
    git-svn-id: http://svn.delph-in.net/lkb/trunk@4096 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/mrs/pet-interface.lisp
+++ src/mrs/pet-interface.lisp
@@ -11,27 +11,37 @@
 (defparameter %pet-types% nil)
 
 (defparameter %pet-features% nil)
+
+(defun pet-clear-symbol-tables ()
+  (setf %pet-types% nil)
+  (setf %pet-features% nil))
 
 (defun vsym (string) 
+  #+:debug
+  (format t "vsym(): `~a'.~%" string)
   (intern (string-upcase string) :mrs))
       
 (defun deref (fs)
+  #+:debug
+  (format t "deref(): ~a.~%" fs)
   (when (fixnump fs) (pet_fs_deref fs)))
 
 (defun cyclic-p (fs)
+  #+:debug
+  (format t "cyclic-p(): ~a.~%" fs)
   (when (fixnump fs) (pet_fs_cyclic_p fs)))
 
 (defun path-value (fs path)
+  #+:debug
+  (format t "path-value(): ~a ~a.~%" fs path)
   (when (fixnump fs)
     (loop
-        with vector = (make-array 
-                       (length path) :element-type 'fixnum
-                       :adjustable nil :fill-pointer 0)
+        with vector = (make-array (length path))
         for i from 0
         for feature in path
         for code = (pet-feature-to-code feature)
         unless code do (return-from path-value)
-        else do (setf (svref vector i) code)
+        else do (setf (aref vector i) code)
         finally (return (pet_fs_path_value fs vector)))))
 
 (defun is-disjunctive-fs (fs)
@@ -39,28 +49,42 @@
   nil)
 
 (defun is-valid-fs (fs)
-  (when (fixnump fs) (pet_fs_valid_p fs)))
+  #+:debug
+  (format t "is-valid-fs(): ~a.~%" fs)
+  (when (fixnump fs) (not (zerop (pet_fs_valid_p fs)))))
 
 (defun fs-arcs (fs)
+  #+:debug
+  (format t "fs-arcs(): ~a.~%" fs)
   (when (fixnump fs)
     (let ((arcs (pet_fs_arcs fs)))
       (loop
           for (feature . value) in arcs
           collect 
-            (cons (pet-code-to-feature feature) (pet-code-to-type value))))))
+            (cons (pet-code-to-feature feature) value)))))
 
 (defun fs-type (fs)
-  (when (fixnump fs) (pet_fs_type fs)))
+  #+:debug
+  (format t "fs-type(): ~a.~%" fs)
+  (when (fixnump fs)
+    (let ((code (pet_fs_type fs)))
+      (unless (= code -1) (pet-code-to-type code)))))
 
 (defun is-top-type (type)
+  #+:debug
+  (format t "is-top-type(): ~a.~%" type)
   (let ((code (pet-type-to-code type)))
     (when (fixnump code) (= code %pet-top%))))
 
 (defun is-valid-type (type)
+  #+:debug
+  (format t "is-valid-type(): ~a.~%" type)
   (let ((code (pet-type-to-code type)))
-    (when (fixnump code) (pet_type_valid_p code))))
+    (when (fixnump code) (not (zerop (pet_type_valid_p code))))))
 
 (defun equal-or-subtype (type1 type2)
+  #+:debug
+  (format t "equal-or-subtype(): ~a ~a.~%" type1 type2)
   ;;
   ;; true if .type1. is identical to .type2. or one of its supertypes.
   ;;
@@ -71,16 +95,20 @@
           (not (zerop (pet_subtype_p code1 code2)))))))
 
 (defun compatible-types (type1 type2)
+  #+:debug
+  (format t "compatible-types(): ~a ~a.~%" type1 type2)
   (or (and (null type1) (null type2))
       (let* ((code1 (pet-type-to-code type1))
              (code2 (pet-type-to-code type2)))
         (when (and (fixnump code1) (fixnump code2))
           (let ((glb (pet_glb code1 code2)))
-            (unless (= glb %pet-bottom%)))))))
+            (unless (= glb %pet-bottom%) (pet-code-to-type glb)))))))
 
 (defun fs-to-mrs (fs)
   (when (fixnump fs)
-    (let ((mrs (extract-mrs-from-fs fs)))
-      (if (mrs-p mrs)
+    (let* ((%pet-types% nil)
+           (%pet-features% nil)
+           (mrs (extract-mrs-from-fs fs)))
+      (if (psoa-p mrs)
         (output-mrs1 mrs 'simple t)
         (format t "fs-to-mrs(): unable to extract MRS from fs # ~a.~%" fs)))))
