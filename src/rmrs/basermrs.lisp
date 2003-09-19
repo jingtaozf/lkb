1b72a6d2cc0b6c93d57a9685dc488df588e80ddc
aac <aac@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-09-19 00:51:20 +0000

    moving some code out of comp to here because it's general purpose
    git-svn-id: http://svn.delph-in.net/lkb/trunk@4177 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ src/rmrs/basermrs.lisp
@@ -0,0 +1,43 @@
+;;; Copyright (c) 2003
+;;;   John Carroll, Ann Copestake, Robert Malouf, Stephan Oepen;
+;;;   see `licence.txt' for conditions.
+
+(in-package :mrs)
+
+;;; RMRS structure
+
+(defstruct (rmrs (:include basemrs))
+  rmrs-args 
+  in-groups
+  bindings
+  cfrom
+  cto
+  origin)
+
+;;; origin values are currently :erg or :rasp - we need this
+;;; because the handling of variables is different
+
+(defstruct rmrs-arg
+  arg-type
+  label
+  val)
+
+(defstruct in-group
+  labels)
+
+;;; the variables are replaced by canonical forms when 
+;;; the structure is printed
+
+(defstruct realpred
+  lemma
+  pos
+  sense)
+
+;;; An RMRS rel has no more than one variable.  In fact, its flist should have
+;;; only one element, which is that variable.  This is assumed
+;;; rather than checked in the following.
+
+(defun retrieve-rmrs-ep-var (ep)
+  (unless (rel-p ep) (error "Incorrect type passed to ep-var"))
+  (car (rel-flist ep)))
+
