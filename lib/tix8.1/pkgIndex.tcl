d351551ccd573f2eef1fbb07f43d5f03e6ebf6a4
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2003-11-18 12:09:29 +0000

    ditch Tix 8.1
    git-svn-id: http://svn.delph-in.net/lkb/trunk@4368 3df82f5b-d43a-0410-af33-fce91db48ec5

--- lib/tix8.1/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-# Tcl package index file, version 1.0
-#
-# $Id$
-#
-
-package ifneeded Tix 8.1.8.3 [list load "[file join [file dirname $dir] libtix8.1.8.3.a]" Tix]
-package ifneeded Tixsam 8.1.8.3 [list load "[file join [file dirname $dir] libtixsam8.1.8.3.a]" Tixsam]
