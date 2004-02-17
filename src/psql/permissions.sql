7972bf022e00f36ed4927fa14ed7d272af0dc2ea
bmw <bmw@3df82f5b-d43a-0410-af33-fce91db48ec5>
2004-02-17 10:54:31 +0000

    remove all reference to db 'erg'
    git-svn-id: http://svn.delph-in.net/lkb/trunk@4594 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/psql/permissions.sql
+++ src/psql/permissions.sql
@@ -1,3 +1,5 @@
+CREATE OR REPLACE FUNCTION public.set_permissions() RETURNS boolean AS '
+BEGIN
 GRANT SELECT ON public.defn TO PUBLIC;
 GRANT SELECT ON public.meta TO PUBLIC;
 GRANT SELECT ON public.multi TO PUBLIC;
@@ -6,7 +8,10 @@ GRANT SELECT ON public.mwe_type TO PUBLIC;
 GRANT SELECT ON public.qry TO PUBLIC;
 GRANT SELECT ON public.qrya TO PUBLIC;
 GRANT SELECT ON public.revision TO PUBLIC;
-
-GRANT CREATE ON DATABASE erg TO PUBLIC;
-
+EXECUTE ''GRANT CREATE ON DATABASE '' || current_database() || '' TO PUBLIC'';
 GRANT INSERT ON public.meta TO PUBLIC;
+RETURN true;
+END;
+' LANGUAGE plpgsql SECURITY DEFINER;
+
+SELECT public.set_permissions();
