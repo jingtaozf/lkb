62e564b5cc55bfb53acfa25c94b429a867729c2c
bmw <bmw@3df82f5b-d43a-0410-af33-fce91db48ec5>
2004-12-07 21:12:26 +0000

    more user friendly
    git-svn-id: http://svn.delph-in.net/lkb/trunk@5115 3df82f5b-d43a-0410-af33-fce91db48ec5

--- lexdb/install-lexdb.sh
+++ lexdb/install-lexdb.sh
@@ -9,5 +9,11 @@ createdb -U lexdb $1
 createlang -U postgres plpgsql $1
 psql -f su-setup.sql -U postgres $1
 psql -f load.sql -U lexdb $1
+psql -c '\copy fields from default-fields.tsv' -U lexdb $1
+if [ -f fields.tsv ]; then 
+    echo 'taking field defns from file fields.tsv';
+    psql -c 'delete from fields' -U lexdb $1; 
+    psql -c '\copy fields from fields.tsv' -U lexdb $1; 
+fi
 psql -f init.sql -U lexdb $1
 
