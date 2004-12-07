ff1faf0da88b6b150c2d861d3ae0ac1144db4610
bmw <bmw@3df82f5b-d43a-0410-af33-fce91db48ec5>
2004-12-07 21:30:20 +0000

    abort nicely in case no fields defn file given
    git-svn-id: http://svn.delph-in.net/lkb/trunk@5116 3df82f5b-d43a-0410-af33-fce91db48ec5

--- lexdb/install-lexdb.sh
+++ lexdb/install-lexdb.sh
@@ -2,18 +2,22 @@
 # Benjamin Waldron;
 # see `licence.txt' for conditions.
 
-# USAGE: install-lexdb DBNAME LEXDB-DIR
+# USAGE: install-lexdb DBNAME FIELDS-FILE
 
 cd $2
 createdb -U lexdb $1
 createlang -U postgres plpgsql $1
 psql -f su-setup.sql -U postgres $1
 psql -f load.sql -U lexdb $1
-psql -c '\copy fields from default-fields.tsv' -U lexdb $1
 if [ -f fields.tsv ]; then 
-    echo 'taking field defns from file fields.tsv';
+    echo "taking field defns from file $2";
     psql -c 'delete from fields' -U lexdb $1; 
-    psql -c '\copy fields from fields.tsv' -U lexdb $1; 
+    psql -c "\copy fields from $2" -U lexdb $1; 
+else
+    echo "ERROR: no field defns file given!";
+    echo "ABORTING..."
+    dropdb -U lexdb $1
+    exit 5;
 fi
 psql -f init.sql -U lexdb $1
 
