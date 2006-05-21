5742198b997884dd2ad2d0d0f7f1549048546197
aac <aac@3df82f5b-d43a-0410-af33-fce91db48ec5>
2006-05-21 19:19:18 +0000

    revised semtest and added a new rasp out fn
    git-svn-id: http://svn.delph-in.net/lkb/trunk@5921 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/rmrs/testing.lisp
+++ src/rmrs/testing.lisp
@@ -11,113 +11,113 @@
 
 (defparameter *rmrs-test-suite*
 '(    
-("It rained" . 1) ; 1
-("Abrams barked" . 1) 
-("The window opened" . 1) 
-("Abrams chased Browne" . 1) 
-("Abrams handed Browne the cigarette" . 1) ; 5
-("Abrams handed the cigarette to Browne" . 1) 
-("Abrams bet Browne a cigarette that it rained" . 1) 
-("Abrams knew that it rained" . 1) 
-("Abrams intended to bark" . 1) 
-("Abrams intended Browne to bark" . 1) 
-("Every cat barked" . 1) ; 11
-("Every cat chased some dog" . 1)
-("My cat barked" . 1)
-("It barked" . 1)
-("The cat chased it" . 1)
-("The cat chased itself" . 1) ; 16
-("The cat chased one" . 1)
-("Mine barked" . 1)
-("That opened" . 1)
-("Cats bark" . 1)
-("Tobacco arrived" . 1) ; 21
-("Some bark" . 1)
-("Some of the cats bark" . 1)
-("No cat barked" . 1)
-("Did the dog bark" . 1)
-("Which dog barked" . 1) ; 26
-("Whose dog barked" . 1)
-("Chase Browne" . 1)
-("Abrams wondered which dog barked" . 1)
-("Abrams wondered whether Browne barked" . 1)
-("The dog that Browne chased barked" . 2) ; 31
-("The dog to chase is barking" . 1)
-("The dog was chased by Browne" . 1)
-("The dog chased by Browne barked" . 2)
-("The dog is barking" . 1)
-("The dog has barked" . 1) ; 36
-("The dog has been barking" . 1)
-("The dog had been barking" . 1)
-("The dog will bark" . 1)
-("The dog is going to bark" . 1)
-("The dog could bark" . 1) ; 41
-("The dog couldn't bark" . 1) ; 
-("The old dog barked" . 1)
-("The dog barked softly" . 1)
-("The dog probably barked" . 1)
-("The dog barked in the garden" . 1) ; 46
-("The dog barks now" . 1)
-("The garden dog barked" . 1)
-("The tobacco garden dog barked" . 2)
-("The cat is old" . 1)
-("The cat is in the garden" . 1) ; 51
-("The barking dog chased Browne" . 1)
-("Chased dogs bark" . 1)
-("Chasing the cat is old" . 1)
-("That the cat chases Browne is old" . 1)
-("Dogs chase whatever barks" . 1) ; 56
-("The dog barked every day" . 1)
-("When did the dog bark" . 3)
-("Three of the dogs bark" . 1)
-("Three bark" . 1)
-("Browne's dog barks" . 1) ; 61
-("Browne's barks" . 2)
-("Twenty three dogs bark" . 1)
-("Two hundred twenty dogs bark" . 1)
-("Abrams arrived by car" . 1)
-("Abrams kept barking" . 1) ; 66
-("Browne squeezed the cat in" . 1)
-("Browne squeezed in the cat" . 1)
-("The picture of Abrams arrived" . 1)
-("Abrams wiped the table clean" . 1)
-("Abrams put Browne in the garden" . 1) ; 71
-("The dog will bark if Browne arrives" . 1)
-("Abrams and Browne arrived" . 1)
-("Abrams, Browne and the dog arrived" . 1)
-("The dog arrived and barked" . 1)
-("The dog arrived and Browne barked" . 1) ; 76
-("The dog barked didn't it" . 1)
-("It is obvious that the dog barked" . 1)
-("Abrams promised Browne to bark" . 1)
-("Abrams seems to bark" . 1)
-("Abrams believes Browne to be barking" . 1) ; 81
-("It bothered Abrams that Browne barked" . 1)
-("It took Abrams ten minutes to arrive" . 2)
-("Abrams left it to Browne to bark" . 2)
-("Abrams strikes Browne as old" . 1)
-("Browne considers Abrams old" . 1) ; 86
-("Abrams liked the idea that Browne could bark" . 1)
-("Abrams barked from ten to three" . 1)
-("Abrams was very old" . 1)
-("Nearly every dog barked" . 1)
-("Abrams barked very softly" . 1) ; 91
-("Browne's chasing of cats bothered Abrams" . 1)
-("It bothered Browne that Abrams chased cats" . 1)
-("June third arrived" . 1)
-("Abrams arrived at three twenty" . 1)
-("Browne arrived on Tuesday morning" . 1) ; 96
-("The cats found a way to bark" . 5)
-("The happier dog chased Browne" . 1)
-("There were cats in the garden" . 1)
-("That dog chased Browne" . 1)
-("Somebody chased Abrams" . 1) ; 101
-("How happy was Abrams" . 1)
-("The number five bothers Browne" . 1)
-("Abrams could" . 1)
-("Browne tried to" . 1)
-("Don't bark" . 1) ; 106
-("The dog arrived barking" . 1)))
+("It rained." . 1) ; 1
+("Abrams barked." . 1) 
+("The window opened." . 1) 
+("Abrams chased Browne." . 1) 
+("Abrams handed Browne the cigarette." . 1) ; 5
+("Abrams handed the cigarette to Browne." . 1) 
+("Abrams bet Browne a cigarette that it rained." . 1) 
+("Abrams knew that it rained." . 1) 
+("Abrams intended to bark." . 1) 
+("Abrams intended Browne to bark." . 1) 
+("Every cat barked." . 1) ; 11
+("Every cat chased some dog." . 2)
+("My cat barked." . 1)
+("It barked." . 1)
+("The cat chased it." . 1)
+("The cat chased itself." . 1) ; 16
+("The cat chased one." . 1)
+("Mine barked." . 1)
+("That opened." . 1)
+("Cats bark." . 1)
+("Tobacco arrived." . 1) ; 21
+("Some bark." . 1)
+("Some of the cats bark." . 1)
+("No cat barked." . 1)
+("Did the dog bark." . 1)
+("Which dog barked." . 1) ; 26
+("Whose dog barked." . 1)
+("Chase Browne." . 1)
+("Abrams wondered which dog barked." . 1)
+("Abrams wondered whether Browne barked." . 1)
+("The dog that Browne chased barked." . 2) ; 31
+("The dog to chase is barking." . 1)
+("The dog was chased by Browne." . 1)
+("The dog chased by Browne barked." . 2)
+("The dog is barking." . 1)
+("The dog has barked." . 1) ; 36
+("The dog has been barking." . 1)
+("The dog had been barking." . 1)
+("The dog will bark." . 1)
+("The dog is going to bark." . 1)
+("The dog could bark." . 1) ; 41
+("The dog couldn't bark." . 1) ; 
+("The old dog barked." . 1)
+("The dog barked softly." . 1)
+("The dog probably barked." . 1)
+("The dog barked in the garden." . 1) ; 46
+("The dog barks now." . 1)
+("The garden dog barked." . 1)
+("The tobacco garden dog barked." . 2)
+("The cat is old." . 1)
+("The cat is in the garden." . 1) ; 51
+("The barking dog chased Browne." . 1)
+("Chased dogs bark." . 1)
+("Chasing the cat is old." . 1)
+("That the cat chases Browne is old." . 1)
+("Dogs chase whatever barks." . 1) ; 56
+("The dog barked every day." . 1)
+("When did the dog bark." . 3)
+("Three of the dogs bark." . 1)
+("Three bark." . 1)
+("Browne's dog barks." . 1) ; 61
+("Browne's barks." . 2)
+("Twenty three dogs bark." . 1)
+("Two hundred twenty dogs bark." . 1)
+("Abrams arrived by car." . 1)
+("Abrams kept barking." . 1) ; 66
+("Browne squeezed the cat in." . 1)
+("Browne squeezed in the cat." . 1)
+("The picture of Abrams arrived." . 1)
+("Abrams wiped the table clean." . 1)
+("Abrams put Browne in the garden." . 1) ; 71
+("The dog will bark if Browne arrives." . 1)
+("Abrams and Browne arrived." . 1)
+("Abrams, Browne and the dog arrived." . 1)
+("The dog arrived and barked." . 1)
+("The dog arrived and Browne barked." . 1) ; 76
+("The dog barked didn't it." . 1)
+("It is obvious that the dog barked." . 1)
+("Abrams promised Browne to bark." . 1)
+("Abrams seems to bark." . 1)
+("Abrams believes Browne to be barking." . 1) ; 81
+("It bothered Abrams that Browne barked." . 1)
+("It took Abrams ten minutes to arrive." . 2)
+("Abrams left it to Browne to bark." . 2)
+("Abrams strikes Browne as old." . 1)
+("Browne considers Abrams old." . 1) ; 86
+("Abrams liked the idea that Browne could bark." . 1)
+("Abrams barked from ten to three." . 1)
+("Abrams was very old." . 1)
+("Nearly every dog barked." . 1)
+("Abrams barked very softly." . 1) ; 91
+("Browne's chasing of cats bothered Abrams." . 1)
+("It bothered Browne that Abrams chased cats." . 1)
+("June third arrived." . 1)
+("Abrams arrived at three twenty." . 1)
+("Browne arrived on Tuesday morning." . 1) ; 96
+("The cats found a way to bark." . 5)
+("The happier dog chased Browne." . 1)
+("There were cats in the garden." . 1)
+("That dog chased Browne." . 1)
+("Somebody chased Abrams." . 1) ; 101
+("How happy was Abrams." . 1)
+("The number five bothers Browne." . 1)
+("Abrams could." . 1)
+("Browne tried to." . 1)
+("Don't bark." . 1) ; 106
+("The dog arrived barking." . 1)))
 
 
 #|
@@ -192,7 +192,7 @@
 	 (parse-number (cdr eg))
          (rasp-rmrs 
           (nth (- egnum 1)
-               (mrs::read-rmrs-file "semtest-rasp.rmrs" :rasp)))
+               (mrs::read-rmrs-file "rasp.rmrs" :rasp)))
 	 (erg-rmrs
 	  (rmrs-for-sentence input parse-number)))
     (dolist (comparison-record (mrs::compare-rmrs erg-rmrs rasp-rmrs strpos-p))
@@ -330,24 +330,52 @@ off, perhaps)
 
 (defun rasp3-out nil
   (let ((*rasp-rmrs-gram-file*
-	(make-pathname 
-	:directory "/homes/aac10/rasp-rmrs/"
-	:name "gram15.rmrs")
+	 #+:mswindows (make-pathname 
+		       :device "c"
+	               :directory "/d/rasp-rmrs/"
+	               :name "gram15.rmrs")
+	#-:mswindows (make-pathname 
+		      :directory "/homes/aac10/rasp-rmrs/"
+		      :name "gram15.rmrs")
 	)
 	(*rasp-rmrs-tag-file*
-	(make-pathname :directory "/homes/aac10/rasp-rmrs/"
-	:name "lex15.rmrs")
+	#+:mswindows (make-pathname 
+		       :device "c"
+	               :directory "/d/rasp-rmrs/"
+	               :name "lex15.rmrs") 
+	#-:mswindows (make-pathname :directory "/homes/aac10/rasp-rmrs/"
+				    :name "lex15.rmrs")
 	)
+	(test-file 
+	 #+:mswindows (make-pathname 
+		       :device "c"
+	               :directory "/d/rasp-rmrs/test-sets/"
+	               :name "annlt.trees") 
+	#-:mswindows (make-pathname :directory "/homes/aac10/rasp-rmrs/test-sets/"
+				    :name "annlt.trees"))
 	(*rasp-xml-word-p* t)
+	(*renumber-hack* t)
 	(*rasp-xml-type* :none))
     (clear-rule-record)
     (read-rmrs-grammar *rasp-rmrs-gram-file*)
     (read-rmrs-tag-templates *rasp-rmrs-tag-file*)
-    (construct-sem-for-tree '(|T/txt-sc1/-+|
- (|S/np_vp|
-  (|NP/n1-name| (|N1/n-name| |<w s='2' e='7'>Abrams_NP1</w>|))
-  (|V1/v| |<w s='9' e='14'>bark+ed_VVD</w>|))
- (|End-punct3/-| |<w s='15' e='15'>._.</w>|)) :rasp t "test")))
+    (with-open-file  (istream test-file
+		      :direction :input)
+    (with-open-file  (ostream "rasp.rmrs"
+		      :direction :output :if-exists :supersede)
+      (format ostream "<rmrs-list>~%")
+      (loop 
+	(let* ((tagged (read istream nil nil))
+	       (number (read istream nil nil))
+	       (tree (read istream nil nil)))
+	  (declare (ignore number))
+	  (unless tree
+	    (return))
+	  (when tree
+	    (construct-sem-for-tree 
+	     tree
+	     :rasp ostream tagged))))
+      (format ostream "</rmrs-list>~%")))))
 
 
 |#
