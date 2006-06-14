736a4697b9f738e8db4af5a11d794b5a43331c4c
bmw <bmw@3df82f5b-d43a-0410-af33-fce91db48ec5>
2006-06-14 13:11:12 +0000

    remove obsolete file
    git-svn-id: http://svn.delph-in.net/lkb/trunk@5953 3df82f5b-d43a-0410-af33-fce91db48ec5

--- lexdb/remove-orthkey-field.el
+++
@@ -1,6 +0,0 @@
-;; to convert older lexdb .rev files to 4.0 spec
-;; works by removing fourth field of dump file
-
-(defun remove-orthkey-field nil
-  (interactive)
-  (replace-regexp "^\\([^\t]*\t[^\t]*\t[^\t]*\t\\)[^\t]*\t" "\\1"))
