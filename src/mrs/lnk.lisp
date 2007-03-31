e99f4a4a646b8597dd7109119f0ef10155168282
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2007-03-31 02:40:12 +0000

    *** empty log message ***
    git-svn-id: http://svn.delph-in.net/lkb/trunk@6203 3df82f5b-d43a-0410-af33-fce91db48ec5

---
+++ src/mrs/lnk.lisp
@@ -0,0 +1,210 @@
+;;; Copyright (c) 1998--2006
+;;;   John Carroll, Ann Copestake, Robert Malouf, Stephan Oepen;
+;;;   see `licence.txt' for conditions.
+
+(in-package :mrs)
+
+;;;
+;;; an attempt at generalizing over various ways of linking to the underlying
+;;; input to the parser, be it by character or vertex ranges (as used at times
+;;; in HoG et al.) or token identifiers (originally at YY and now in LOGON).
+;;; currently, there are four distinct value formats:
+;;;
+;;;   <0:4>    character range (i.e. a sub-string of an assumed flat input);
+;;;   <0#2>    chart vertex range (traditional in PET to some degree);
+;;;   <0 1 3>  token identifiers, i.e. links to basic input units;
+;;;   <@42>    edge identifier (used internally in generation)
+;;;
+;;; of these, the first is maybe most widely supported across DELPH-IN tools,
+;;; while the second (in my view) should be deprecated.  the third resembles
+;;; what was used in VerbMobil, YY, and now LOGON; given that the input to a
+;;; `deep' parser can always be viewed as a token lattice, this is probably the
+;;; most general mode, and we should aim to establish it over time: first, the
+;;; underlying input may not have been string-shaped (but come from the lattice
+;;; of a speech recognizer), and second even with one underlying string there
+;;; could be token-level ambiguity, sp identifying the actual token used in an
+;;; analysis preserves more information.  properties like the sub-string range,
+;;; prosodic information (VerbMobil), or pointers to KB nodes (YY) can all be
+;;; associated with the individual tokens sent into the parser.  finally, the
+;;; fourth mode is used in generation, where surface linking actually is a two-
+;;; stage process (see comments in `generate.lsp').              (4-dec-06; oe)
+;;;
+(defun output-lnk (lnk &key (stream t))
+  (if stream
+    (when *show-lnk-p*
+      (case (first lnk)
+        (:characters
+         (let ((start (second lnk))
+               (end (third lnk)))
+           (when (and (numberp start) (numberp end)
+                      (>= start 0) (>= end 0))
+             (format stream "<~a:~a>" (second lnk) (third lnk)))))
+        (:vertices
+         (format stream "<~a#~a>" (second lnk) (third lnk)))
+        (:tokens
+         (format stream "<~{~a~^ ~}>" (rest lnk)))
+        (:id
+         (format stream "<@~a>" (second lnk)))))
+    (with-output-to-string (stream)
+      (output-lnk lnk :stream stream))))
+
+(defun read-lnk (&optional (stream t))
+  (ignore-errors
+   (when (and (streamp stream) (open-stream-p stream))
+     (let ((c (peek-char t stream nil nil))
+           (*readtable* (copy-readtable))
+           type)
+       (set-syntax-from-char #\: #\" *readtable*)
+       (set-syntax-from-char #\# #\" *readtable*)
+       (set-syntax-from-char #\, #\" *readtable*)
+       (set-syntax-from-char #\> #\" *readtable*)
+       (when (and c (char= c #\<))
+         (read-char stream nil nil)
+         (let ((c (peek-char t stream nil nil)))
+           (when (and c (member c '(#\@) :test #'char=))
+             (read-char stream nil nil)
+             (setf type :id)))
+         (let ((start (read stream nil nil)))
+           (when (numberp start)
+             (let ((c (peek-char t stream nil nil)))
+               (cond
+                ((null c) (return-from read-lnk))
+                ((member c '(#\: #\#) :test #'char=)
+                 (read-char stream nil nil)
+                 (let ((end (read stream nil nil)))
+                   (when (numberp end)
+                     (let ((foo (read-char stream nil nil)))
+                       (when (and foo (char= foo #\>))
+                         (list
+                          (if (char= c #\:) :characters :vertices)
+                          start end))))))
+                (t
+                 (loop
+                     with ids = (list start)
+                     for c = (peek-char t stream nil nil)
+                     for id = (unless (and c (char= c #\>))
+                                (read stream nil nil))
+                     when (and c (char= c #\>))
+                     return (cons (or type :tokens) (nreverse ids))
+                     unless (numberp id) do (return-from read-lnk)
+                     do (push id ids))))))))))))
+                     
+(defun combine-lnks (lnk1 lnk2)
+  (cond
+   ((null lnk1) lnk2)
+   ((null lnk2) lnk1)
+   ((eq (first lnk1) (first lnk2))
+    (case (first lnk1)
+      ((:characters :vertices)
+       (list
+        (first lnk1)
+        (min (second lnk1) (second lnk2)) (max (third lnk1) (third lnk2))))
+      (:tokens
+       (list :tokens (union (rest lnk1) (rest lnk2))))))))
+
+(defun compute-lnk-distortion (pairs &key tags)
+  (declare (ignore tags))
+  ;;
+  ;; when pair-wise comparing EPs from the same MRS, we distinguish as follows:
+  ;;   0: identical span
+  ;;   1 or -1: inclusion or backward inclusion
+  ;;   2 or -2: adjacent precedence (or inverse)
+  ;;   3 or -3: non-adjacent precedence
+  ;;
+  (labels ((adjacentp (i j)
+             (<= (abs (- i j)) 1))
+           (compare-lnks (lnk1 lnk2)
+             (when (and (eq :characters (first lnk1))
+                        (eq :characters (first lnk2)))
+               (let ((start1 (second lnk1))
+                     (end1 (third lnk1))
+                     (start2 (second lnk2))
+                     (end2 (third lnk2)))
+                 (cond
+                  ((and (adjacentp start1 start2) (adjacentp end1 end2)) 0)
+                  ((and (adjacentp start1 start2) (> end1 end2)) 1)
+                  ((and (< start1 start2) (adjacentp end1 end2)) 1)
+                  ((and (adjacentp start1 start2) (< end1 end2) -1))
+                  ((and (> start1 start2) (adjacentp end1 end2)) -1)
+                  ((adjacentp end1 start2) 2)
+                  ((adjacentp start1 end2) -2)
+                  ((< end1 start2) 3)
+                  ((> start1 end2) -3)
+                  (t 0))))))
+    
+    (loop
+        with n = (length pairs)
+        with distortion = 0
+        with eps1 = (loop for foo in pairs collect (first foo))
+        with eps2 = (loop for foo in pairs collect (rest foo))
+        for key in eps1
+        for klnk = (rel-lnk key)
+        for match in eps2
+        for mlnk = (rel-lnk match)
+        do
+          (loop
+              for ep1 in eps1
+              for ep2 in eps2
+              unless (and (eq key ep1) (eq match ep2))
+              do
+                (let* ((key (compare-lnks klnk (rel-lnk ep1)))
+                       (match (compare-lnks mlnk (rel-lnk ep2))))
+                  (when (and key match)
+                    (incf distortion (abs (- key match))))))
+        finally (return (if (zerop n) 0 (float (/ distortion n)))))))
+
+(in-package :lkb)
+
+#+:lkb
+(defun lnk-tdfs (tdfs lnks)
+  (unless (consp mrs::*rel-lnk-path*)
+    (format
+     t
+     "lnk-tdfs(): invalid value for *rel-lnk-path* (~a)."
+     mrs::*rel-lnk-path*)
+    (return-from lnk-tdfs tdfs))
+  
+  (if *within-unification-context-p*
+    (let* ((unifications
+            (loop
+                for lnk in lnks
+                for value = (if (stringp lnk) lnk (write-to-string lnk))
+                for path = *list-head* then (append *list-tail* path)
+                collect
+                  (make-unification
+                   :lhs (create-path-from-feature-list path)
+                   :rhs (make-u-value :type value))
+                into unifications
+                finally
+                  (let ((path (nconc (butlast path) *list-tail*)))
+                    (return
+                      (cons
+                       (make-unification
+                        :lhs (create-path-from-feature-list path)
+                        :rhs (make-u-value :type *empty-list-type*))
+                       unifications)))))
+           (overlay (process-unifications unifications nil))
+           (overlay (and overlay (create-wffs overlay nil)))
+           (path
+            (append mrs::*initial-semantics-path* mrs::*psoa-liszt-path*))
+           (rels (unify-existing-dag-at-end-of (tdfs-indef tdfs) path)))
+      (when (and overlay rels)
+        (loop
+            with visited
+            with *unify-wffs* = t
+            for path = *list-head* then (append *list-tail* path)
+            for lnk = (unify-existing-dag-at-end-of
+                       rels (append path mrs::*rel-lnk-path*))
+            ;;
+            ;; protect ourselves against cyclic RELS feature structures (which
+            ;; might have cropped up during unification but have yet to be 
+            ;; found by the copier.
+            ;;
+            while (and lnk (not (member lnk visited :test #'eq)))
+            do (push lnk visited)
+            when (eq (unify-get-type lnk) *list-type*)
+            do (unify-dags lnk overlay)))
+      tdfs)
+    (with-unification-context (null)
+      (let ((result (lnk-tdfs tdfs lnks)))
+        (copy-tdfs-elements result)))))
---
+++ src/mt/smt.lisp
@@ -0,0 +1,244 @@
+(in-package :mt)
+
+;;;
+;;; Copyright (c) 2007 -- 2007 Stephan Oepen (oe@csli.stanford.edu)
+;;; Copyright (c) 2007 -- 2007 Erik Velldal (erikve@ifi.uio.no)
+;;;
+;;; This program is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU Lesser General Public License as published by
+;;; the Free Software Foundation; either version 2.1 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but WITHOUT
+;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+;;; License for more details.
+;;; 
+
+(defparameter *smt-directory*
+  #+:logon
+  (namestring (dir-append (get-sources-dir "mt") '(:relative "mt")))
+  #-:logon
+  nil)
+
+(defparameter *smt* nil)
+
+(defstruct smt
+  lfn lnf mlfn mlnf)
+
+(defstruct wp
+  in out weight)
+
+(defun read-smt (&key file hash)
+  (if file
+    (multiple-value-bind (input foo pid)
+        (run-process
+         (format nil "gzip -d -c '~a'" (namestring file))
+         :wait nil
+         :output :stream :input "/dev/null"
+         :error-output "/dev/null" :if-error-output-exists :append)
+      (declare (ignore foo))
+      (let* ((file (pathname file))
+             (name (format 
+                    nil 
+                    "~a~@[.~a~]"
+                    (pathname-name file) (pathname-type file))))
+        (format t "read-smt(): reading file `~a' ... " name)
+        (loop
+            for line = (read-line input nil nil)
+            while line do
+              (multiple-value-bind (foo matches)
+                  (cl-ppcre:scan-to-strings "^([^ ]+) ([^ ]+) ([^ ]+)$" line)
+                (declare (ignore foo))
+                (when (= (array-total-size matches) 3)
+                  ;;
+                  ;; _fix_me_
+                  ;; given the file names i received from erik, it looks like 
+                  ;; the order on each line is reversed, i.e. out <-- in?
+                  ;;                                            (5-jan-07; oe)
+                  (let* ((in (aref matches 1))
+                         (in (unless (string= in "NULL") in))
+                         (out (aref matches 0))
+                         (out (unless (string= out "NULL") out))
+                         (weight
+                          (ignore-errors (read-from-string (aref matches 2)))))
+                    (push
+                     (make-wp :in in :out out :weight weight)
+                     (gethash in hash))))))
+        (let ((n (hash-table-count hash)))
+          (format t "(~a pair~p).~%" n n ))
+        (loop
+            for key being each hash-key using (hash-value value) in hash
+            do (setf (gethash key hash) (sort value #'> :key #'wp-weight)))
+        #+:allegro
+        (sys:os-wait nil pid)
+        hash))
+    (setf *smt*
+      (when *smt-directory*
+        (let ((lfn
+               (make-pathname
+                :directory *smt-directory* :name "lex.f2n" :type "gz"))
+              (lnf
+               (make-pathname
+                :directory *smt-directory* :name "lex.n2f" :type "gz"))
+              (smt (make-smt)))
+          (when (probe-file lfn)
+            (setf (smt-lfn smt)
+              (read-smt :file lfn :hash (make-hash-table :test #'equal)))
+            (when (probe-file lnf)
+              (setf (smt-lnf smt)
+                (read-smt :file lnf :hash (make-hash-table :test #'equal)))
+              (setf (smt-mlfn smt)
+                (loop
+                    for wps being each hash-value in (smt-lfn smt)
+                    minimize (loop
+                                 for wp in wps minimize (wp-weight wp))))
+              (setf (smt-mlnf smt)
+                (loop
+                    for wps being each hash-value in (smt-lnf smt)
+                    minimize (loop
+                                 for wp in wps minimize (wp-weight wp))))
+              smt)))))))
+
+(defun smt-score-strings (source targets &key (smt *smt*) weight)
+  (unless (smt-p smt)
+    (return-from smt-score-strings
+      (loop
+          repeat (length targets)
+          collect (if (numberp weight) 0 (cons 0 0)))))
+  (labels ((smt-score-pair (source target &key (direction :forward))
+             (when (or (null source) (null target))
+               (return-from smt-score-pair 0))
+             (when (eq direction :backward)
+               (let ((foo source))
+                 (setf source target)
+                 (setf target foo)))
+             (loop
+                 with alignments = (make-array (length target)) with deletions
+                 for stoken in source
+                 for wps
+                 = (gethash
+                    stoken
+                    (if (eq direction :forward) (smt-lfn smt) (smt-lnf smt)))
+                 do
+                   (loop
+                       for wp in wps
+                       for out = (wp-out wp)
+                       for match
+                       = (and out (position out target :test #'string=))
+                       when (null out)
+                       return (push (wp-weight wp) deletions)
+                       when match
+                       return (push (wp-weight wp) (aref alignments match))
+                       finally
+                         (if (eq direction :forward)
+                           (push (smt-mlfn smt) deletions)
+                           (push (smt-mlnf smt) deletions)))
+                 finally
+                   (return
+                     (let ((product (apply #'* deletions)))
+                       (loop
+                           for alignment across alignments
+                           for sum = (apply #'+ alignment)
+                           when alignment
+                           do (setf product
+                                (* product (/ sum (length alignment)))))
+                       (if (zerop product)
+                         0
+                         (let ((n (+ (length deletions)
+                                     (length (remove nil alignments)))))
+                           (/ (log product) n))))))))
+    (let* ((strings (scrub-strings (cons source targets)))
+           (source (ppcre:split "[ \\t]+" (first strings)))
+           (targets (loop
+                        for target in (rest strings)
+                        collect (ppcre:split "[ \\t]+" target))))
+      (loop
+          for target in targets
+          for forward = (smt-score-pair source target)
+          for backward = (smt-score-pair source target :direction :backward)
+          collect (if (numberp weight)
+                    (+ (* forward weight) (* backward (- 1 weight)))
+                    (cons forward backward))))))
+
+(defparameter *smt-binary*
+  #+:logon
+  (let* ((root (getenv "LOGONROOT"))
+         (root (and root (parse-namestring root))))
+    (when root (format nil "~a/bin/pharaoh" root)))
+  #-:logon
+  nil)
+
+(defparameter *smt-configuration*
+  #+:logon
+  (namestring (make-pathname 
+               :directory (namestring
+                           (dir-append 
+                            (get-sources-dir "mt") '(:relative "mt")))
+               :name "pharaoh.ini"))
+  #-:logon
+  nil)
+
+(defparameter *smt-input* nil)
+
+(defparameter *smt-output* 
+  (format nil "/tmp/.smt.~a.~a.out" (lkb::current-user) (lkb::current-pid)))
+
+(defparameter *smt-log* 
+  (format nil "/tmp/.smt.~a.~a.log" (lkb::current-user) (lkb::current-pid)))
+
+(defparameter *smt-pid* nil)
+      
+(let ((lock (mp:make-process-lock)))
+
+  (defun smt-initialize (&optional (configuration *smt-configuration*))
+    (when (and *smt-binary* configuration)
+      (mp:with-process-lock (lock)
+        (with-open-file (stream *smt-output*
+                         :direction :output :if-exists :supersede))
+        (with-open-file (stream *smt-log*
+                         :direction :output :if-exists :supersede))
+        (let* ((directory (pathname-directory configuration))
+               (directory (namestring (make-pathname :directory directory)))
+               (command (format
+                        nil
+                        "cd '~a' && ~a -f '~a'"
+                        directory *smt-binary* configuration))
+              foo)
+          (multiple-value-setq (*smt-input* foo *smt-pid*)
+            (run-process 
+             command
+             :wait nil
+             :input :stream
+             :output *smt-output* :if-output-exists :supersede
+             :error-output *smt-log* :if-error-output-exists :supersede))
+          (setf foo foo))))
+    (sleep 0.5))
+  
+  (defun smt-translate-strings (strings &key (wait 30))
+    (if *smt-binary*
+      (mp:with-process-lock (lock)
+        (when (null *smt-input*) (smt-initialize))
+        (loop
+            for string in (scrub-strings strings)
+            do
+              (format *smt-input* "<s> ~a </s>~%" (lm-normalize-string string))
+            finally
+              (close *smt-input*)
+              (setf *smt-input* nil)
+              #+:allegro
+              (sys:os-wait nil *smt-pid*))
+        (when (and (numberp wait) (> wait 0))
+          (with-open-file (stream *smt-output*
+                           :direction :input :if-does-not-exist nil)
+            (loop
+                while (and (zerop (file-length stream)) (> wait 0))
+                do (sleep 1))))
+        (let ((results
+               (with-open-file (stream *smt-output* :direction :input)
+                 (loop
+                     for line = (read-line stream nil nil)
+                     while line collect line))))
+          (smt-initialize)
+          results))
+      strings)))
