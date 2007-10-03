ee1e2393e428d2d86a2bc8d2e5da46828d71f901
cj <cj@3df82f5b-d43a-0410-af33-fce91db48ec5>
2007-10-03 13:55:52 +0000

    Removed unnecessary pre-loading of code defined in build_standalone.lsp
    git-svn-id: http://svn.delph-in.net/lkb/trunk@6289 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/rmrs/rasp3/build_standalone_image.sh
+++ src/rmrs/rasp3/build_standalone_image.sh
@@ -2,10 +2,6 @@
 
  ( \
    echo "(load \"$1/src/general/loadup.lisp\")"; \
-   echo "(pushnew :pxml *features*)"; \
-   echo "(compile-system \"rmrs\" :force t)"; \
-   echo "(in-package :mrs)";\
-   echo "(load \"$1/src/rmrs/rasp3/standalone-server.lsp\")"; \
    echo "(load \"$1/src/rmrs/rasp3/standalone-image.lsp\")"; \
    echo "(excl:exit)"; \
  ) | ( ACL_LOCALE=C  /usr/opt/acl80.64/alisp -I /usr/opt/acl80.64/alisp.dxl -qq; )
