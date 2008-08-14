403eaf3e594ab2b867a1869eb6a28a55dfb30f09
cj <cj@3df82f5b-d43a-0410-af33-fce91db48ec5>
2008-08-14 10:37:00 +0000

    Commented lines for linking a copy of the shared object file.
    git-svn-id: http://svn.delph-in.net/lkb/trunk@6352 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/rmrs/rasp3/build_standalone_image.sh
+++ src/rmrs/rasp3/build_standalone_image.sh
@@ -4,10 +4,10 @@
    echo "(load \"$1/src/general/loadup.lisp\")"; \
    echo "(load \"$1/src/rmrs/rasp3/standalone-image.lsp\")"; \
    echo "(excl:exit)"; \
- ) | ( ACL_LOCALE=C  /usr/opt/acl80.64/alisp -I /usr/opt/acl80.64/alisp.dxl -qq; )
+ ) | ( ACL_LOCALE=C  /anfs/repl/acl81.64/alisp -I /anfs/repl/acl81.64/alisp.dxl -qq; )
 
-mv $1/rasp3-rmrs/libacli8010.so $1/rasp3-rmrs/libacli8010.so_orig;
-ln -s /usr/opt/acl80.64/libacli8010.so $1/rasp3-rmrs;
+##mv $1/rasp3-rmrs/libacli8010.so $1/rasp3-rmrs/libacli8010.so_orig;
+##ln -s /usr/opt/acl80.64/libacli8010.so $1/rasp3-rmrs;
 
 
 
