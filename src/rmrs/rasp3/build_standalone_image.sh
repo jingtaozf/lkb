3e71a5b5631ec9df30fec005489366340793687f
cj <cj@3df82f5b-d43a-0410-af33-fce91db48ec5>
2008-12-10 11:40:19 +0000

    Script made more general.
    git-svn-id: http://svn.delph-in.net/lkb/trunk@6372 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/rmrs/rasp3/build_standalone_image.sh
+++ src/rmrs/rasp3/build_standalone_image.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+## Modify as necessary:
+acl_home=/anfs/repl/acl81.64
+
  ( \
    echo "(load \"$1/src/general/loadup.lisp\")"; \
    echo "(load \"$1/src/rmrs/rasp3/standalone-image.lsp\")"; \
    echo "(excl:exit)"; \
- ) | ( ACL_LOCALE=C  /anfs/repl/acl81.64/alisp -I /anfs/repl/acl81.64/alisp.dxl -qq; )
+ ) | ( ACL_LOCALE=C $acl_home/alisp -I $acl_home/alisp.dxl -qq; )
 
-##mv $1/rasp3-rmrs/libacli8010.so $1/rasp3-rmrs/libacli8010.so_orig;
-##ln -s /usr/opt/acl80.64/libacli8010.so $1/rasp3-rmrs;
 
 
 
