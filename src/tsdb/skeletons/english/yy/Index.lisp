0646831504fb1eef1281d77ff6c2ca6973cdc11e
oe <oe@3df82f5b-d43a-0410-af33-fce91db48ec5>
2010-06-08 09:34:53 +0000

    thin out LKB `trunk' collection of English skeletons; less duplication
    git-svn-id: http://svn.delph-in.net/lkb/trunk@7918 3df82f5b-d43a-0410-af33-fce91db48ec5

--- src/tsdb/skeletons/english/yy/Index.lisp
+++
@@ -1,5 +0,0 @@
-(((:path . "cell") (:content . "DT Cell Phone Groups (Development Section)"))
- ((:path . "ecpa") (:content . "Ecommerce Email (Product Availability)"))
- ((:path . "ecos") (:content . "Ecommerce Email (Order Status)"))
- ((:path . "ecoc") (:content . "Ecommerce Email (Order Cancellation)"))
- ((:path . "ecpr") (:content . "Ecommerce Email (Product Return)")))
